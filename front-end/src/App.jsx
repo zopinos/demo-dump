f3ed97ddbf58bd151c09ffa89ca4ad575ce5ba03
zopinos <zopi.tv@gmail.com>
2024-08-21 12:33:12 +0300

    Routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Routes/Home';
+import First from './components/Routes/First';
 
-function App() {
+const App = () => {
 
   return (
-    <>
-      <h1>Hello World!</h1>
+    <Router>
+      <div>
+        <Link to='/'>home</Link>
+        <Link to='/1'>firts thing</Link>
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/1' element={<First />} />
+      </Routes>
+
       <p>
         This is some text.
       </p>
-    </>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ front-end/src/components/Routes/First.jsx
@@ -0,0 +1,9 @@
+const First = () => {
+  return (
+    <>
+      <h1>This would be the first demo</h1>
+    </>
+  );
+};
+
+export default First;
