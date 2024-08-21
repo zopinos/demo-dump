106e53894d4652c82fc4377db71b590e816290a9
zopinos <zopi.tv@gmail.com>
2024-08-21 13:53:14 +0300

    Styled components and theme

---
+++ front-end/src/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+  colors: {
+    black:        'black',
+    white:        'white',
+    gray:         '#101010',
+    light:        '#B3C6D5',
+    accentLight:  '#7391AA',
+    main:         '#C9E7FF',
+    secondary:    '#416380',
+    accentDark:   '#1D3C55',
+    dark:         '#092033',
+  },
+  fontsSizes: {
+    body: '16px',
+  }
+};
+
+/*
+light       #B3C6D5
+semi-light  #7391AA
+main        #C9E7FF
+secondary   #416380
+accent      #1D3C55
+dark        #092033
+*/
+
+export default theme;
