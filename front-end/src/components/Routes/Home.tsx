5605b6971e134668203913c8a6dbe78b78c89aec
zopinos <zopi.tv@gmail.com>
2025-02-02 00:20:57 +0200

    Add style

--- front-end/src/components/Routes/Home.tsx
+++ front-end/src/components/Routes/Home.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { H1, P } from "../StyledElements";
 
-const Div = styled.div`
+const Base = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
@@ -10,12 +10,31 @@ const Div = styled.div`
   background-color: ${({ theme }) => theme.palette.primary};
 `;
 
+const Page = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 30em;
+`;
+
+const Img = styled.img`
+  position: fixed;
+  top: 90px;
+  left: 49%;
+  height: 150px;
+`;
+
 const Home = () => {
   return (
-    <Div>
-      <H1>Hello World!</H1>
-      <P>I made this website to display things I have played around with.</P>
-    </Div>
+    <Base>
+      <Img draggable="false" src="src/assets/20250201_233506_2.png" />
+      <Page>
+        <H1>Demo Dump</H1>
+        <P>Hello and welcome to demo dump! This is a collection of stuff (mostly visual) I have made.</P>
+      </Page>
+    </Base>
   );
 };
 
