f2dd5e3bec37ef2f5402b550d9b78a32d10b0618
zopinos <zopi.tv@gmail.com>
2026-01-11 16:34:29 +0200

    Update

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,6 +5,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isProduction ? "/front-end/dist/" : "/",
+  base: isProduction ? "/demo-dump/" : "/",
   plugins: [react()]
 });
